0e7763eefc626e3db672d122a8153d6dc9697880
suryansh9315 <suryansh.sharma9315@gmail.com>
2022-07-20 14:04:30 +0530

    .

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
+const withTM = require('next-transpile-modules')([
+  "three","@react-spring/three","@react-three/fiber"
+])
 
-module.exports = nextConfig
+module.exports = withTM,nextConfig
